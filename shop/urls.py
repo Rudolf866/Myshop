6568b28640ecf1fe4ff5d6f94434304b1d7f00e1
Rudolf <kovalevskiy2017@outlook.com>
2021-01-24 19:59:18 +0300

    'firstCommit'

---
+++ shop/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.urls import path
+
+from shop import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list ,name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
