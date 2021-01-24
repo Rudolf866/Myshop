2c57b8c7f76cd93f4cb42fe32a4db4efa8de6230
Rudolf <kovalevskiy2017@outlook.com>
2021-01-25 00:08:05 +0300

    'Реализация

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from cart.forms import CartAddProductForm
 from shop.models import Category, Product
 
 
@@ -17,4 +18,5 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
